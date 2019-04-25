6d6707735ab6e8f63bdcab9b6f541e0902937bdf
tstuefe <thomas.stuefe@gmail.com>
2019-04-25 13:47:33 +0200

    remodel ClassLifeCycle.java

--- repros8/src/main/java/de/stuefe/repros/metaspace/ClassLifeCycle.java
+++ repros8/src/main/java/de/stuefe/repros/metaspace/ClassLifeCycle.java
@@ -3,20 +3,49 @@ package de.stuefe.repros.metaspace;
 import de.stuefe.repros.MiscUtils;
 import de.stuefe.repros.metaspace.internals.InMemoryClassLoader;
 import de.stuefe.repros.metaspace.internals.Utils;
+import de.stuefe.repros.util.MyTestCaseBase;
+import org.apache.commons.cli.Option;
 
-public class ClassLifeCycle {
+import java.util.ArrayList;
+
+public class ClassLifeCycle extends MyTestCaseBase {
 
     public static void main(String args[]) throws Exception {
+        ClassLifeCycle test = new ClassLifeCycle();
+        test.run(args);
+    }
 
-        int numClasses = 50;
-        int sizeFactor = 10;
+    Option options_num_classes =
+            Option.builder()
+                    .longOpt("num-classes-per-loader")
+                    .hasArg().type(Long.class)
+                    .desc("number of classes per loader")
+                    .build();
 
-        if (args.length > 0) {
-            numClasses = Integer.parseInt(args[0]);
-            if (args.length > 1) {
-                sizeFactor = Integer.parseInt(args[1]);
-            }
-        }
+    Option options_size_classes =
+            Option.builder()
+                    .longOpt("class-size")
+                    .hasArg().type(Long.class)
+                    .desc("avg class size factor")
+                    .build();
+
+    class LoaderGeneration {
+        ArrayList<ClassLoader> loaders = new ArrayList<>();
+        ArrayList<Class> loaded_classes = new ArrayList<>();
+    }
+
+    ;
+
+    private void run(String[] args) throws Exception {
+
+        Option[] options = new Option[]{
+                options_num_classes,
+                options_size_classes};
+
+        prolog(getClass(), args, options);
+
+        int numClasses = Integer.parseInt(options_num_classes.getValue("100"));
+        int sizeFactor = Integer.parseInt(options_size_classes.getValue("10"));
 
         System.out.print("Generating " + numClasses + " in memory class files, size factor "  + sizeFactor + " ...");
         for (int j = 0; j < numClasses; j++) {
@@ -24,23 +53,25 @@ public class ClassLifeCycle {
             Utils.createRandomClass(className, sizeFactor);
         }
 
-        MiscUtils.waitForKeyPress("Before loading...");
+        waitForKeyPress("Before creating loader...");
 
         ClassLoader loader = new InMemoryClassLoader("myloader", null);
+
+        waitForKeyPress("Before loading...");
 
         for (int i = 0; i < numClasses; i ++) {
             // Loading classes...
             Class<?> clazz = Class.forName("testclass_" + i, true, loader);
         }
 
-        MiscUtils.waitForKeyPress("After loading");
+        waitForKeyPress("After loading");
 
         loader = null;
 
-        MiscUtils.waitForKeyPress("Before gc");
+        waitForKeyPress("Before gc");
 
         System.gc(); System.gc();
-        MiscUtils.waitForKeyPress("After gc");
+        waitForKeyPress("After gc");
 
     }
 
