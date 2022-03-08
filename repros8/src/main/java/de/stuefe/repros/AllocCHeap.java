8bb0128e2bee80be9e3bb6fefedd48c66cfebae3
tstuefe <thomas.stuefe@gmail.com>
2022-03-08 07:52:09 +0100

    AllocCHeap changes

--- repros8/src/main/java/de/stuefe/repros/AllocCHeap.java
+++ repros8/src/main/java/de/stuefe/repros/AllocCHeap.java
@@ -11,33 +11,49 @@ import java.util.concurrent.Callable;
         description = "Allocate C Heap; optionally leak or peak.")
 public class AllocCHeap extends TestCaseBase implements Callable<Integer> {
 
-    @CommandLine.Option(names = { "--auto-yes", "-y" }, defaultValue = "false",
+    @CommandLine.Option(names = { "--auto-yes", "-y" },
             description = "Autoyes.")
-    boolean auto_yes;
+    boolean auto_yes = false;
 
-    @CommandLine.Option(names = { "--waitsecs" }, defaultValue = "0",
+    @CommandLine.Option(names = { "--waitsecs" },
             description = "If autoyes, how many seconds to wait per step (default: ${DEFAULT-VALUE}).")
-    int waitsecs;
+    int waitsecs = 0;
 
-    @CommandLine.Option(names = { "--nowait" }, defaultValue = "false",
-            description = "do not wait (only with autoyes).0")
-    boolean nowait;
+    @CommandLine.Option(names = { "--nowait" },
+            description = "do not wait (only with autoyes).")
+    boolean nowait = false;
 
     @CommandLine.Option(names = { "--size" },
             description = "Allocation size (default: ${DEFAULT-VALUE})")
     int allocationSize = 8;
 
+    @CommandLine.Option(names = { "--randfactor" },
+            description = "Randomize size between [size..size * randfactor) (default: ${DEFAULT-VALUE})")
+    int randfactor = 1;
+
+    @CommandLine.Option(names = { "--randseed" },
+            description = "If not 0, fixed randomizer seed (default: ${DEFAULT-VALUE}).")
+    long randseed = 0;
+
     @CommandLine.Option(names = { "--num" },
             description = "Number of allocations (default: ${DEFAULT-VALUE})")
     int numAllocations = 1024*1024*64;
+
+    @CommandLine.Option(names = { "--allocdelay" },
+            description = "Delay, in about 0.1 ns, between subsequent mallocs (default: ${DEFAULT-VALUE}).")
+    int alloc_delay = 0;
+
+    @CommandLine.Option(names = { "--freedelay" },
+            description = "Delay, in about 0.1 ns, between subsequent frees (default: ${DEFAULT-VALUE}).")
+    int free_delay = 0;
 
     @CommandLine.Option(names = { "--cycles" },
             description = "Number of repeats (default: ${DEFAULT-VALUE})")
     int numCycles = 3;
 
-    @CommandLine.Option(names = { "--touch" },
-            description = "Touch touch? (default: ${DEFAULT-VALUE})")
-    boolean touch = true;
+    @CommandLine.Option(names = { "--notouch" },
+            description = "By default, we touch allocated memory. Can be switched off with --notouch (default: ${DEFAULT-VALUE}).")
+    boolean notouch = false;
 
     enum TestType { peak, leak };
     @CommandLine.Option(names = { "--type" },
@@ -56,7 +72,7 @@ public class AllocCHeap extends TestCaseBase implements Callable<Integer> {
 
     static void touchMemory(Unsafe unsafe, long address, int size) {
         for(long p = address; p < (address + size); p += 4096) {
-            unsafe.putByte(p, (byte)1);
+            unsafe.putLong(p, p);
         }
     }
 
@@ -83,17 +99,48 @@ public class AllocCHeap extends TestCaseBase implements Callable<Integer> {
         }
     }
 
+    void sleep_delay(int ms) {
+        try {
+           Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    int randomized_allocation_size() {
+        if (randfactor != 1) {
+            int lower_bound = allocationSize;
+            int upper_bound = allocationSize * randfactor;
+            int randsize = random.nextInt(upper_bound - lower_bound) + lower_bound;
+            return randsize;
+        }
+        return allocationSize;
+    }
+
+    void seed_the_randomizer() {
+        if (randseed == 0) {
+            randseed = random.nextLong();
+        }
+        random.setSeed(randseed);
+    }
+
     @Override
     public Integer call() throws Exception {
 
         initialize(false, auto_yes, nowait);
 
+        seed_the_randomizer();
+
         System.out.println("Number of Cylces: " + numCycles);
         System.out.println("Number of Allocations: " + numAllocations);
         System.out.println("Allocation Size: " + allocationSize);
-        System.out.println("Touch: " + touch);
+        System.out.println("Size Random factor: " + randfactor);
+        System.out.println("Touch: " + !notouch);
         System.out.println("Type: " + testType);
         System.out.println("Noise: " + noise);
+        System.out.println("Randomizer seed: " + randseed);
+        System.out.println("Alloc delay: " + alloc_delay);
+        System.out.println("Free delay: " + free_delay);
 
         Field f = Unsafe.class.getDeclaredField("theUnsafe");
         f.setAccessible(true);
@@ -114,13 +161,16 @@ public class AllocCHeap extends TestCaseBase implements Callable<Integer> {
             waitForKeyPress("Cycle " + cycle + ": before allocation...", waitsecs);
 
             for (int n = 0; n < numAllocations; n++) {
-                long p = theUnsafe.allocateMemory(allocationSize);
+                long p = theUnsafe.allocateMemory(randomized_allocation_size());
                 if (p == 0) {
                     throw new RuntimeException("malloc failed");
                 }
                 ptrs[n] = p;
-                if (touch) {
-                    touchMemory(theUnsafe, p, allocationSize);
+                if (!notouch) {
+                    touchMemory(theUnsafe, p, randomized_allocation_size());
+                }
+                if ((n % 10000) == 0) {
+                    sleep_delay(alloc_delay);
                 }
             }
 
@@ -131,6 +181,9 @@ public class AllocCHeap extends TestCaseBase implements Callable<Integer> {
                 for (int n = 0; n < numAllocations; n++) {
                     theUnsafe.freeMemory(ptrs[n]);
                     ptrs[n] = 0;
+                    if ((n % 10000) == 0) {
+                        sleep_delay(free_delay);
+                    }
                 }
                 waitForKeyPress("Cycle " + cycle + ": free phase completed.", 0);
             }
