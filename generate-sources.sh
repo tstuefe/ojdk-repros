5b94156ef3d48e82d7ee78a7b002f9b01ff9046c
tstuefe <thomas.stuefe@gmail.com>
2024-07-22 16:02:05 +0200

    generate-sources.sh

---
+++ generate-sources.sh
@@ -0,0 +1,19 @@
+NUM_SOURCES=16384
+OUTDIR=./generated-test-sources
+JARFILE=TESTDATA.jar
+
+rm -rf $OUTDIR
+
+set -e
+
+java -cp $REPROS_JAR de.stuefe.repros.metaspace.GenerateSources -C ${NUM_SOURCES} -D $OUTDIR
+pushd $OUTDIR
+javac *.java
+jar -cf $JARFILE *.class
+popd
+
+JAR=`realpath ./${OUTDIR}/${JARFILE}`
+
+echo "generated $JAR "
+ls -alt $JAR
+
